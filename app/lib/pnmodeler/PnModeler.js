2d38f45b6b2e8bf69546d5883a056873a49a36e7
MaximilianKoenig <koenigmax1@web.de>
2024-04-07 23:17:43 +0200

    Dump

--- app/lib/pnmodeler/PnModeler.js
+++ app/lib/pnmodeler/PnModeler.js
@@ -62,13 +62,14 @@ import PnModdle from './moddle';
 // TODO
 const emptyDiagram =
     `<?xml version="1.0" encoding="UTF-8"?>
-<pn:definitions xmlns:pn="http://bptlab/schema/pn" xmlns:pnDi="http://bptlab/schema/pnDi">
-    <pn:PetriNet id="test" name="One">
-        <pn:place name="Foo" id="Place_0" type="pn:Place" x="119" y="177" />
-        <pn:Transition name="Bar" id="Transition_0" type="pn:Transition" x="289" y="177" />
-        <pn:Arc id="Arc_0" type="pn:Arc" source="Place_0" target="Transition_0" />
-    </pn:PetriNet>
-</pn:definitions>`;
+<ptn:definitions xmlns:ptn="http://bpt-lab.org/schemas/ptn" xmlns:ptnDi="http://bpt-lab.org/schemas/ptnDi" xmlns:dc="https://www.omg.org/spec/BPMN/20100501/DC.xsd">
+    <ptn:ptNet id="ptNet_1" name="Place Transition Net 1">
+    </ptn:ptNet>
+    <ptnDi:ptnDiagram id="ptNet_1_di">
+    <ptnDi:ptnPlane id="ptNet_1_plane" ptNet="ptNet_1">
+    </ptnDi:ptnPlane>
+    </ptnDi:ptnDiagram>
+</ptn:definitions>`;
 
 export default function PnModeler(options) {
     const {
@@ -139,15 +140,18 @@ PnModeler.prototype.createNew = function () {
 
 PnModeler.prototype.importXML = function (xml) {
     const self = this;
+    console.log(xml);
 
     return new Promise(function (resolve, reject) {
         // hook in pre-parse listeners +
         // allow xml manipulation
         xml = self._emit('import.parse.start', {xml: xml}) || xml;
+        console.log(self.get('moddle'));
 
-        self.get('moddle').fromXML(xml).then(function (result) {
+        self.get('moddle').fromXML(xml, 'ptn:Definitions').then(function (result) {
             let definitions = result.rootElement;
             const { references, warnings, elementsById } = result;
+            console.log(result);
 
             const context = {
                 references,
@@ -155,15 +159,16 @@ PnModeler.prototype.importXML = function (xml) {
                 warnings
             };
 
-            for (let id in elementsById) {
-                self.get('elementFactory')._ids.claim(id, elementsById[id]);
-            }
+            // for (let id in elementsById) {
+            //     self.get('elementFactory')._ids.claim(id, elementsById[id]);
+            // }
 
             definitions = self._emit('import.parse.complete', {
                 definitions,
                 context
             }) || definitions;
 
+            console.log(definitions);
             self.importDefinitions(definitions);
             self._emit('import.done', {error: null, warnings: null});
             resolve();
@@ -181,66 +186,66 @@ PnModeler.prototype.importXML = function (xml) {
 }
 
 PnModeler.prototype.importDefinitions = function (definitions) {
-    this.get('elementFactory')._ids.clear();
+    // this.get('elementFactory')._ids.clear();
     this._definitions = definitions;
     this._emit('import.render.start', {definitions: definitions});
-    this.showPn(definitions.petriNets[0]);
-    this._emit('import.render.complete', {});
+    // this.showPn(definitions.petriNets[0]);
+    // this._emit('import.render.complete', {});
 }
 
-PnModeler.prototype.showPn = function (petriNet) {
-    this.clear();
-    this._petriNet = petriNet;
-    if (petriNet) {
-        const elementFactory = this.get('elementFactory');
-        const daigramRoot = elementFactory.createRoot({ type: 'pn:PetriNet', businessObject: petriNet });
-        const canvas = this.get('canvas');
-        canvas.setRootElement(daigramRoot);
+// PnModeler.prototype.showPn = function (petriNet) {
+//     this.clear();
+//     this._petriNet = petriNet;
+//     if (petriNet) {
+//         const elementFactory = this.get('elementFactory');
+//         const daigramRoot = elementFactory.createRoot({ type: 'ptn:PetriNet', businessObject: petriNet });
+//         const canvas = this.get('canvas');
+//         canvas.setRootElement(daigramRoot);
 
-        const elements = groupBy(petriNet.get('Elements'), element => element.$type);
-        const places = {};
-        const transitions = {};
+//         const elements = groupBy(petriNet.get('Elements'), element => element.$type);
+//         const places = {};
+//         const transitions = {};
 
-        (elements['pn:Place'] || []).forEach(place => {
-            const placeVisual = elementFactory.createShape({
-                type: 'pn:Place',
-                businessObject: place,
-                x: parseInt(place.get('x')),
-                y: parseInt(place.get('y'))
-            });
-            places[place.id] = placeVisual;
-            canvas.addShape(placeVisual, daigramRoot);
-        });
+//         (elements['ptn:Place'] || []).forEach(place => {
+//             const placeVisual = elementFactory.createShape({
+//                 type: 'ptn:Place',
+//                 businessObject: place,
+//                 x: parseInt(place.get('x')),
+//                 y: parseInt(place.get('y'))
+//             });
+//             places[place.id] = placeVisual;
+//             canvas.addShape(placeVisual, daigramRoot);
+//         });
 
-        (elements['pn:Transition'] || []).forEach(transition => {
-            const transitionVisual = elementFactory.createShape({
-                type: 'pn:Transition',
-                businessObject: transition,
-                x: parseInt(transition.get('x')),
-                y: parseInt(transition.get('y'))
-            });
-            transitions[transition.id] = transitionVisual;
-            canvas.addShape(transitionVisual, daigramRoot);
-        });
+//         (elements['ptn:Transition'] || []).forEach(transition => {
+//             const transitionVisual = elementFactory.createShape({
+//                 type: 'ptn:Transition',
+//                 businessObject: transition,
+//                 x: parseInt(transition.get('x')),
+//                 y: parseInt(transition.get('y'))
+//             });
+//             transitions[transition.id] = transitionVisual;
+//             canvas.addShape(transitionVisual, daigramRoot);
+//         });
 
-        (elements['pn:Arc'] || []).forEach(arc => {
-            const sourceId = arc.get('source').get('id');
-            const targetId = arc.get('target').get('id');
+//         (elements['ptn:Arc'] || []).forEach(arc => {
+//             const sourceId = arc.get('source').get('id');
+//             const targetId = arc.get('target').get('id');
 
-            const source = places[sourceId] || transitions[sourceId];
-            const target = places[targetId] || transitions[targetId];
+//             const source = places[sourceId] || transitions[sourceId];
+//             const target = places[targetId] || transitions[targetId];
             
-            const arcVisual = elementFactory.createConnection({
-                type: 'pn:Arc',
-                businessObject: arc,
-                source,
-                target,
-                waypoints: this.get('pnUpdater').connectionWaypoints(source, target)
-            });
-            canvas.addConnection(arcVisual, daigramRoot);
-        });
-    }
-}
+//             const arcVisual = elementFactory.createConnection({
+//                 type: 'ptn:Arc',
+//                 businessObject: arc,
+//                 source,
+//                 target,
+//                 waypoints: this.get('pnUpdater').connectionWaypoints(source, target)
+//             });
+//             canvas.addConnection(arcVisual, daigramRoot);
+//         });
+//     }
+// }
 
 PnModeler.prototype.saveXML = function (options) {
     options = options || {};
