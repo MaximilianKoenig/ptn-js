e36b215463e6902b553b8b97e496389453fec6a7
MaximilianKoenig <koenigmax1@web.de>
2025-02-23 12:58:33 +0100

    Rename moddle elements generically

--- assets/emptyDiagram.js
+++ assets/emptyDiagram.js
@@ -1,11 +1,11 @@
 const emptyDiagram = `<?xml version="1.0" encoding="UTF-8"?>
 <ptn:definitions xmlns:ptn="http://bpt-lab.org/schemas/ptn" xmlns:ptnDi="http://bpt-lab.org/schemas/ptnDi" xmlns:dc="https://www.omg.org/spec/BPMN/20100501/DC.xsd">
-    <ptn:ptNet id="ptNet_1" name="Place Transition Net 1">
-    </ptn:ptNet>
-    <ptnDi:ptnDiagram id="ptNet_1_di">
-        <ptnDi:ptnPlane id="ptNet_1_plane" ptnElement="ptNet_1">
-        </ptnDi:ptnPlane>
-    </ptnDi:ptnDiagram>
+    <ptn:model id="ptNet_1" name="Place Transition Net 1">
+    </ptn:model>
+    <ptnDi:diagram id="ptNet_1_di">
+        <ptnDi:plane id="ptNet_1_plane" modelElement="ptNet_1">
+        </ptnDi:plane>
+    </ptnDi:diagram>
 </ptn:definitions>`;
 
 export default emptyDiagram;
