ca925a1f8bd2b6c21f2cfefc77bd923eb8816667
MaximilianKoenig <koenigmax1@web.de>
2025-02-23 11:56:18 +0100

    Remove pn-modeler specific modules and functions

---
+++ assets/emptyDiagram.js
@@ -0,0 +1,11 @@
+const emptyDiagram = `<?xml version="1.0" encoding="UTF-8"?>
+<ptn:definitions xmlns:ptn="http://bpt-lab.org/schemas/ptn" xmlns:ptnDi="http://bpt-lab.org/schemas/ptnDi" xmlns:dc="https://www.omg.org/spec/BPMN/20100501/DC.xsd">
+    <ptn:ptNet id="ptNet_1" name="Place Transition Net 1">
+    </ptn:ptNet>
+    <ptnDi:ptnDiagram id="ptNet_1_di">
+        <ptnDi:ptnPlane id="ptNet_1_plane" ptnElement="ptNet_1">
+        </ptnDi:ptnPlane>
+    </ptnDi:ptnDiagram>
+</ptn:definitions>`;
+
+export default emptyDiagram;
